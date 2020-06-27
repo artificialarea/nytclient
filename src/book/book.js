f65cf1e7913e5bf4c241220a018c87ff926354e9
Sacha Sedriks <sachased@gmail.com>
2020-06-27 10:52:41 -0700

    set up template so can fetch from my localhost:8000 node express server.

---
+++ src/book/book.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './book.css';
+import moment from 'moment';
+
+// Since the job of the Book component is to only display the details and there are no interactions this can be a plain function component (book.js not Book.js)
+
+export default function Book(props) {
+  return (
+    <div className="book">
+      <h2>{ props.title }</h2>
+      <div className="book_author">by { props.author }</div>
+      <div className="book_publisher">
+        Published by: { props.publisher }
+        on {moment(props.published_date).format('DD MMM YYYY')}
+      </div>
+      <div className="book_description">{ props.description }</div>
+      <div className="book_details">
+        Rank {props.rank} this week
+      </div>
+    </div>
+  );
+}
